abf0aa9ea2de6ee9114ca07102421a2ed9ea569b
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-01 11:51:06 +0530

    First Commit

---
+++ Introduction.java
@@ -0,0 +1,27 @@
+public class Introduction{
+    public static void main(String[] args) {
+        System.out.println("Hello Yash");
+    }
+}
+
+
+
+
+
+
+/*
+ * class main{    // this is the class, we can write many of the functions in the one class
+ *      void main(){
+ *              // this is the function, Here We can write a code 
+ *     }
+ * }
+ */
+
+// Below are the process of the How to compiler Run the code
+
+// In the java There aretwo things to run the code 
+//     1. Compiler 
+//     2. Execution
+
+//     1). Compiler == Source code(.java) -> Compiler(Which is in JDK) -> ByteCode(.class) [ByteCode i.e, .class file will be run on the any of the devices like Mac os , Linux etc - That's Why java is called as portable]
+//     2). Execution == ByteCode(.class file) -> JVM -> Convert into "Native Code" (Which is in Binary (0 or 1) form) [Any Of the Machine will be easily Understand this Native/Binary Code]
