113f53852f61d9fc86daa073dc2679a46a5a8fbf
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-05 20:22:36 +0530

    Updated

--- Patterns_2.java
+++ Patterns_2.java
@@ -289,14 +289,13 @@ public class Patterns_2 {
         int n=5;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                // Check for main diagonal elements
                 if (i == j) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
                 }
             }
-            System.out.println();
+            System.out.println("\n");
         }
 
     }
