d92153cd484a29e699b61eead7ef2682c6b15977
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-05 20:14:25 +0530

    Final Commmit

--- Functions.java
+++ Functions.java
@@ -32,3 +32,6 @@ public class Functions {
         System.out.println(factorial(number));
     }
 }
+
+
+// Interview Question Link(ChatGPT) : https://chatgpt.com/share/6774e194-12d8-8006-a63c-0448b45f0480
