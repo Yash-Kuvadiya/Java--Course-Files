ccd003a9007a383facdf08a11a60456644ac192d
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-07 11:12:46 +0530

    Final Commit

--- array.java
+++ array.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class array {
     public static void main(String[] args) {
         
         // Java array called as the "zero-indexed array" because in java index of array is starting from 0.
 
-
-        
+        /* 
+        // this is the static array
+       // int myArray[] = {10,20,30};
         int myArray[] = new int[3];
         myArray[0]=10;
         myArray[1]=20;
@@ -18,7 +21,28 @@ public class array {
         for(int i=0;i<=3;i++){
             System.out.println("Printed Array : "+myArray[i]);
         }
-        
+        */
+
+        //Dynamic Array
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Define Size of Array :");
+        int size = sc.nextInt();
+
+        int myArray[] = new int[size]; // array declaration
+
+        // Insertion of array
+        System.out.println("Enter the values in array :");
+        for(int i=0;i<size;i++){
+            myArray[i] = sc.nextInt();
+        }
+
+        //display array
+        System.out.println("Your Array is:");
+        for(int i=0;i<size;i++){
+            System.out.println(myArray[i]);
+        }
+
+
 
 
 
