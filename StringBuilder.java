add2304548b7377e263670ea1e557eb7e3fabbb4
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-07 19:53:02 +0530

    Final Commit

---
+++ StringBuilder.java
@@ -0,0 +1,20 @@
+import java.util.*;;
+public class StringBuilder {
+    public static void main(String[] args) {
+        
+        /*#NOTE : 
+         *  JAVA Ma String Mutable Hovathi tema insert , upadate and delete karvu possible nthiii . 
+         * Therefor we will use the String Builder , - aano Use karta aapne string ne easily 
+         * CRUD kari shakiye chiye.
+         */
+
+         // STRING BUILDER no use niche mujab declare kari ne kari shakay 6e
+        
+         StringBuilder sb = new StringBuilder();
+         System.out.println(sb);
+
+
+         // For More content and Example : https://www.scaler.com/topics/java/stringbuilder-in-java/
+
+    }
+}
