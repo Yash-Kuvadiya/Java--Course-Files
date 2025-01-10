058041bb96977473917b2c89e7e99ed6cdc75506
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-10 21:06:41 +0530

    Final Commit

---
+++ constructor.java
@@ -0,0 +1,18 @@
+/*Constructor name must be the same as its class name.
+A Constructor must have no explicit return type.
+A Java constructor cannot be abstract, static, final, and synchronized.*/
+
+class Bike1{
+    // default constructor & Non-parametrized Constructor
+    /*Bike1(){
+        System.out.println("THis is Deafult Constructor");
+    }*/
+    
+
+    // Here Main Method also comes
+}
+
+public class constructor { 
+    //Bike1 b1 = new Bike1(); // object created 
+    // at the time of object create , constructor Bike() also called and print the message 
+}
