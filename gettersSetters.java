362eab13436d16dc041277069624cb939dbcd49b
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-12 19:08:28 +0530

    Final Commit

---
+++ gettersSetters.java
@@ -0,0 +1,17 @@
+public class gettersSetters {
+
+    class employee{
+        private int id;
+        private String name;
+        
+        public void setName(String name){    // setter - which is set name here...
+            this.name = name;
+        }
+        public String getName(){    // getter- for the name, which is alsways return value 
+            return name;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
