867fdeaee1a3b77ab2d7b008f5e1f04b1bf10961
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-12 19:23:01 +0530

    Final Commit

---
+++ accessModifier.java
@@ -0,0 +1,18 @@
+/*
+ * ------------------------------------------------------------------------------------ 
+ * Modifier 	|    Description
+ * ------------------------------------------------------------------------------------
+    Default 	-   declarations are visible only within the package (package private)
+    Private 	-   declarations are visible within the class only
+    Protected   -	declarations are visible within the package or all subclasses
+    Public      -	declarations are visible everywhere
+    ------------------------------------------------------------------------------------
+ * 
+ */
+
+
+public class accessModifier {
+    public static void main(String[] args) {
+        
+    }
+}
