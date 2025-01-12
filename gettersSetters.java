cab6657b1976b43cab9c3e16ba584e72dd2a51c8
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-12 19:15:56 +0530

    Final Commit

--- gettersSetters.java
+++ gettersSetters.java
@@ -1,17 +1,41 @@
-public class gettersSetters {
+class employee{
+    private int id;
+    private String name;
+    
 
-    class employee{
-        private int id;
-        private String name;
-        
-        public void setName(String name){    // setter - which is set name here...
-            this.name = name;
-        }
-        public String getName(){    // getter- for the name, which is alsways return value 
-            return name;
-        }
+    /*
+    -jyare variable private ma declare thaya hoi tyare tene direct accesss object through naa kari shakay. 
+    - Tyrae through method ae variable ne access karva pade 6e. je getter & setters 6e.
+    -getter is always return something.
+    */ 
+    
+    //for name
+    public void setName(String name){    // setter - which is set name here...
+        this.name = name;
     }
+    public String getName(){    // getter- for the name, which is alsways return value 
+        return name;
+    }
+
+    //for id
+    public void setId(int id){
+        this.id=id;
+    }
+    public int getId(){
+        return id;
+    }
+}
+
+public class gettersSetters {
     public static void main(String[] args) {
-        
+        employee e1=new employee();
+
+        //for name
+        e1.setName("Yash"); // call setter
+        System.out.println(e1.getName()); // print name through getter
+
+        //for id
+        e1.setId(10);
+        System.out.println(e1.getId());
     }
 }
