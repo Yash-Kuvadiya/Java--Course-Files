cbab13f656e6a705ae7c7bc6af8679f3c9199a95
Yash-Kuvadiya <yashkuvadiya19@gmail.com>
2025-01-14 20:11:46 +0530

    Final Commit

--- methodOverriding.java
+++ methodOverriding.java
@@ -21,6 +21,12 @@ class AXIS extends Bank{
 
 public class methodOverriding {
     public static void main(String[] args) {
-        
+        Bank b;  
+        b=new SBI();  
+        System.out.println("SBI Rate of Interest: "+b.getRateOfInterest());  
+        b=new ICICI();  
+        System.out.println("ICICI Rate of Interest: "+b.getRateOfInterest());  
+        b=new AXIS();  
+        System.out.println("AXIS Rate of Interest: "+b.getRateOfInterest()); 
     }
 }
